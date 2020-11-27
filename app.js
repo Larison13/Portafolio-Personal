3753e25f8d96aa559f022943db59df2f21bf6dc7
Larison <larison.sepulveda01@gmail.com>
2020-11-26 21:44:03 -0400

    uodate 2.4

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById('btnscrolltop')
+
+    addEventListener('click', function(){
+
+        window.scrollTo({
+     
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
